c437250dec25ddfcfe387978ef4a5d1a31ef2be0
HarelYagen <154447275+HarelYagen@users.noreply.github.com>
2024-03-07 19:03:32 +0200

    Update List.java

--- List.java
+++ List.java
@@ -68,10 +68,15 @@ public class List {
     /** If the given character exists in one of the CharData objects in this list,
      *  increments its counter. Otherwise, adds a new CharData object with the
      *  given chr to the beginning of this list. */
-       public void update(char chr) {
+        public void update(char chr) {
         int i = indexOf(chr);
-        if (i != -1) get(i).count++; 
-        else addFirst(chr);
+        if ( i > -1 ) { 
+            ListIterator iterator = listIterator(i);
+            Node temp = iterator.current;
+            temp.cp.count++;
+        } else {
+            addFirst(chr);
+        }
     }
 
 
